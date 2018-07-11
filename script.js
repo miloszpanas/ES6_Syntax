15de8a2c04513894b0217b385bc68d1036417a8b
Miłosz Panas <miloszpanas@gmail.com>
2018-07-11 09:06:33 +0200

    Add solved ES6 tasks

--- script.js
+++ script.js
@@ -11,13 +11,16 @@ const multiply = (a, b = 1) => console.log(a*b);
 
 // Zadanie trzecie
 
-// nie wiem jak to napisać w tym przypadku, kiedy nie podaje określonej tablicy z numerami.
-// Z określoną tablicą zrobibym to tak:
+const avg = (...data) => data.reduce((prev, curr) => prev + curr) / data.length;
+
+console.log(avg(2,3,34,5,6));
 
 
 // zadanie czwarte
 
-// waiting
+const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
+
+console.log(avg(...grades));
 
 // zadanie piąte
 
